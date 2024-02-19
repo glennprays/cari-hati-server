b800c3608a20ae1eb26eb70f419f554b634f5a9e
glennprays <glennsagala@gmail.com>
2024-02-19 18:35:55 +0700

    init project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { ChatModule } from './chat/chat.module';
+import { GiftModule } from './gift/gift.module';
+import { NotificationModule } from './notification/notification.module';
+import { CoinModule } from './coin/coin.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, CommonModule, ChatModule, GiftModule, NotificationModule, CoinModule],
   controllers: [AppController],
   providers: [AppService],
 })
