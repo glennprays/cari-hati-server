c4e65dad6d2196c0e45bea24f845ec8143d12208
glennprays <88752553+glennprays@users.noreply.github.com>
2024-02-19 22:42:01 +0700

    Revert "Develop (#9)"
    This reverts commit 96e51d7e3c0d1bb8ffdc960bf614a4fed032c191.

--- src/gift/gift.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SendModule } from './send/send.module';
-import { ReceivedModule } from './received/received.module';
-
-@Module({
-  imports: [SendModule, ReceivedModule]
-})
-export class GiftModule {}
