c6cc12ccf28da9e86ff58dff61edf38e0455a362
glennprays <glennsagala@gmail.com>
2024-02-19 19:06:23 +0700

    feature: mongo config connection

--- src/common/database/mongo/mongo.module.ts
+++ src/common/database/mongo/mongo.module.ts
@@ -1,4 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import mongoConfig from './mongo.config';
 
-@Module({})
-export class MongoModule {}
+@Module({
+    imports: [
+        MongooseModule.forRootAsync({
+            useFactory: () => ({
+                uri: mongoConfig.uri,
+            }),
+        }),
+    ],
+})
+export class MongoModule implements OnModuleInit {
+    async onModuleInit() {
+        try {
+            // Attempt to establish a connection
+            await mongoose.connect(mongoConfig.uri);
+
+            // Log a success message if the connection is successful
+            console.log('Connected to MongoDB');
+        } catch (error) {
+            // Log an error message if the connection fails
+            console.error('Error connecting to MongoDB:', error.message);
+        }
+    }
+}
