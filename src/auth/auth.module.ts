52367e86f0b20bd67012b13334338b8a29f32c61
glennprays <glennsagala@gmail.com>
2024-02-21 17:36:59 +0700

    update: re-structuring project directory

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login/login.service';
 import { LogoutService } from './logout/logout.service';
+import { EmailVerificationModule } from './email-verification/email-verification.module';
+import { PasswordResetService } from './password-reset/password-reset.service';
+import { RegistrationService } from './registration/registration.service';
 
 @Module({
-  providers: [LoginService, LogoutService]
+  providers: [LoginService, LogoutService, PasswordResetService, RegistrationService],
+  imports: [EmailVerificationModule]
 })
 export class AuthModule {}
