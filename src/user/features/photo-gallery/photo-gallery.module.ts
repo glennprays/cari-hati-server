09c09fb1e960fbef3567026f751eff84dccf3f25
glennprays <glennsagala@gmail.com>
2024-02-21 17:36:59 +0700

    update: re-structuring project directory

---
+++ src/user/features/photo-gallery/photo-gallery.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { PhotoGalleryService } from './photo-gallery.service';
+
+@Module({
+  providers: [PhotoGalleryService]
+})
+export class PhotoGalleryModule {}
