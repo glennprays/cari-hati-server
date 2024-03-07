4246a2c050bd8c95d0686bd90d8c74e05126c96b
glennprays <glennsagala@gmail.com>
2024-03-07 19:03:27 +0700

    unfinish seeding

--- src/database/seeding/factories/person.factory.ts
+++ src/database/seeding/factories/person.factory.ts
@@ -2,7 +2,7 @@ import { Faker } from '@faker-js/faker';
 import { setSeederFactory } from 'typeorm-extension';
 import { Person, Role } from 'src/user/entities/person.entity';
 
-export const PersonFactory = setSeederFactory(Person, async (faker: Faker) => {
+export default setSeederFactory(Person, async (faker: Faker) => {
     const person = new Person();
     person.email = faker.internet.email();
     person.role = Role.USER;
--- src/database/seeds-config.ts
+++ src/database/seeds-config.ts
@@ -3,41 +3,17 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '/../../.env' });
 
 import 'reflect-metadata';
-import { DataSource, DataSourceOptions } from 'typeorm';
-import { runSeeders, SeederOptions } from 'typeorm-extension';
+import { DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
+import TransactionSeeder from './seeding/seeds/transaction.seeder';
 
-const options: DataSourceOptions & SeederOptions = {
+export const dataSourceOptions: DataSourceOptions & SeederOptions = {
     type: 'postgres',
     host: process.env.PG_DB_HOST,
     port: parseInt(process.env.PG_DB_PORT, 10) || 5432,
     username: process.env.PG_DB_USER,
     password: process.env.PG_DB_PASSWORD,
     database: process.env.PG_DB_DATABASE,
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    factories: [__dirname + '/factories/*.factory{.ts,.js}'],
-    seeds: [__dirname + '/seeds/*.seed{.ts,.js}'],
+    synchronize: false,
+    seeds: [TransactionSeeder]
 };
-
-export const dataSource = new DataSource(options);
-
-// (async () => {
-//     const options: DataSourceOptions  = {
-//         type: 'postgres',
-//         host: process.env.PG_DB_HOST,
-//         port: parseInt(process.env.PG_DB_PORT, 10) || 5432,
-//         username: process.env.PG_DB_USER,
-//         password: process.env.PG_DB_PASSWORD,
-//         database: process.env.PG_DB_DATABASE,
-//         synchronize: false,
-//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-//     };
-
-//     const dataSource = new DataSource(options);
-//     console.log('Initializing data source');
-//     await dataSource.initialize();
-
-//     await runSeeders(dataSource, {
-//         factories: [__dirname + '/factories/*.factory{.ts,.js}'],
-//         seeds: [__dirname + '/seeds/*.seed{.ts,.js}'],
-//     });
-// })();
