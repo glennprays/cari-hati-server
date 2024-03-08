562b884216c3c9813e5743bc42c7846bd98826b6
glennprays <glennsagala@gmail.com>
2024-03-08 08:24:34 +0700

    unfinish seeder

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,12 +1,8 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: __dirname + '/../../.env' });
-
 import 'reflect-metadata';
-import { DataSourceOptions } from 'typeorm';
-import { SeederOptions } from 'typeorm-extension';
-
-export const dataSourceOptions: DataSourceOptions & SeederOptions = {
+export default {
     type: 'postgres',
     host: process.env.PG_DB_HOST,
     port: parseInt(process.env.PG_DB_PORT, 10) || 5432,
@@ -14,4 +10,6 @@ export const dataSourceOptions: DataSourceOptions & SeederOptions = {
     password: process.env.PG_DB_PASSWORD,
     database: process.env.PG_DB_DATABASE,
     synchronize: false,
+    seeds: ['src/database/seeding/seeds/**/*{.ts,.js}'],
+    factories: ['src/database/seeding/factories/**/*{.ts,.js}']
 };
