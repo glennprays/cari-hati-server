9bb8e81fcb384a2e8ae6c1d6dbbbefdeb0343a78
glennprays <glennsagala@gmail.com>
2024-03-13 18:52:42 +0700

    fix: remove unused mongoose lib

--- src/chat/models/message.interface.ts
+++ src/chat/models/message.interface.ts
@@ -1,8 +1,6 @@
-import { Schema } from 'mongoose';
-
 export interface Message {
-    conversationId: Schema.Types.ObjectId;
-    sender: Schema.Types.ObjectId;
+    conversationId: string;
+    sender: string;
     text: string;
     imageAttachment?: string;
     createdAt: Date;
