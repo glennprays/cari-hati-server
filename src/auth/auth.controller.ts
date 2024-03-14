e30cfa4074cc560723bacdf4698923107e50a5d6
glennprays <glennsagala@gmail.com>
2024-03-14 10:44:10 +0700

    add firebase creds dir

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,13 @@ import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthDto } from './dtos/auth.dto';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/localAuth.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    // @UseGuards(LocalAuthGuard)
+    // @UseGuards(AuthGuard('local'))
     @Post('signin')
     async signIn(@Body() data: AuthDto) {
         return await this.authService.signIn(data);
