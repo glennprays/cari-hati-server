1b289e1f2ec2cd509982581c55bfc61914d920f2
glennprays <glennsagala@gmail.com>
2024-03-19 01:02:14 +0700

    fix: account activation bug; modify database when activated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Post,
+    Request,
+    UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { JwtRefreshGuard } from './guards/jwt-refresh-auth.guard';
-import { EmailDTO, RegisterPersonDTO } from './dtos/email.dto';
+import { RegisterPersonDTO } from './dtos/email.dto';
 import { PersonService } from 'src/user/services/person.service';
+import { JwtGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -24,25 +32,29 @@ export class AuthController {
         return await this.authService.refreshToken(req.user);
     }
 
-    // DEBUG: DELETE THIS. THIS IS JUST A TESTING EMAIL SEND
-    @Post('signup')
-    async signUp(@Body() emailDto: EmailDTO) {
-        return await this.authService.sendVerificationEmail(emailDto.email);
-    }
-
-    // FIXME: Get the email from the JWT token authorization header
+    @UseGuards(JwtGuard)
     @Post('account/activate')
-    async activateAccount(@Body() codeDto: { code: number; email: string }) {
+    async activateAccount(@Body() codeDto: { code: number }, @Request() req) {
         return await this.authService.activateAccount(
-            codeDto.email,
+            req.user.username,
             codeDto.code,
         );
     }
 
     @Post('register')
     async register(@Body() registerPersonDTO: RegisterPersonDTO) {
-        const person = await this.personService.inputPerson(registerPersonDTO.email, registerPersonDTO.password);
-        await this.authService.sendVerificationEmail(registerPersonDTO.email)
-        return await this.authService.generateToken(person)
+        const person = await this.personService.inputPerson(
+            registerPersonDTO.email,
+            registerPersonDTO.password,
+        );
+        const status = await this.authService.sendVerificationEmail(
+            registerPersonDTO.email,
+        );
+        if (!status) {
+            throw new BadRequestException(
+                'Email activation not sent, please login to resend',
+            );
+        }
+        return await this.authService.generateToken(person);
     }
 }
