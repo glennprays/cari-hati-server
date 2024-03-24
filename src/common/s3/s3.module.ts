efb089d0112f849aa478c96135b9456fbe1cb0ed
glennprays <glennsagala@gmail.com>
2024-03-24 23:52:49 +0700

    feature: unfinished s3 config

--- src/common/s3/s3.module.ts
+++ src/common/s3/s3.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { S3Service } from './s3.service';
+import { S3Client } from '@aws-sdk/client-s3';
 
 @Module({
-    providers: [S3Service],
+    providers: [{
+        provide: 'S3_CLIENT',
+        useFactory: () => {
+            return new S3Client
+        }
+    }, S3Service],
     exports: [S3Service],
 })
 export class S3Module {}
