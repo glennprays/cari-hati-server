92aa1f7c5ea32228d9bce7e96967588ba5b495de
michaelsetiawan <michaelsetiawan686@gmail.com>
2024-03-28 12:48:36 +0700

    feature: move match function to feature/match; fix variable not used

---
+++ src/user/features/match/match.controller.ts
@@ -0,0 +1,48 @@
+import { Controller, Post, UseGuards, Request, Get, Delete, Body } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UserGetAllMatchDTO, UserRequestMatchDTO, UserUnmatchMatchDTO } from 'src/user/dtos/user.dto';
+import { MatchService } from './match.service';
+
+@Controller('')
+export class MatchController {
+    constructor(private matchService: MatchService) {}
+
+    @UseGuards(JwtGuard)
+    @Post('')
+    async requestMatch(
+        @Body() userRequestMatchDTO: UserRequestMatchDTO,
+        @Request() req,
+    ) {
+        return await this.matchService.userRequestMatch(
+            req.user.sub.id,
+            userRequestMatchDTO.receiverId,
+            userRequestMatchDTO.liked,
+        );
+    }
+
+    @UseGuards(JwtGuard)    
+    @Get('')
+    async getUserMatch(
+        @Body() userGetAllMatchDTO: UserGetAllMatchDTO,
+        @Request() req
+        ) {
+            console.log("masuk");
+
+        return this.matchService.findAllMatchesByUserId(
+            req.user.sub.id,
+            userGetAllMatchDTO.accepted_only,
+            );
+    }
+    
+    @UseGuards(JwtGuard)
+    @Delete('')
+    async unmatchWithUser(
+        @Body() userUnmatchtMatchDTO: UserUnmatchMatchDTO,
+        @Request() req,
+        ) {
+        return this.matchService.unmatchWithUser(
+            req.user.sub.id,
+            userUnmatchtMatchDTO.receiverId,
+            );
+    }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,17 +7,29 @@ import { DatabaseModule } from 'src/common/database/database.module';
 import { UserService } from './services/user.service';
 import { UserController } from './user.controller';
 import { S3Module } from 'src/common/s3/s3.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
     providers: [PersonService, UserService],
     exports: [PersonService, UserService],
     imports: [
-        MatchModule,
         PhotoGalleryModule,
         BlockModule,
         DatabaseModule,
         S3Module,
-    ],
+        RouterModule.register([
+            {
+              path: 'users',
+              module: UserModule,
+              children: [
+                {
+                  path: 'matches',
+                  module: MatchModule,
+                },
+              ],
+            },
+          ])
+        ],
     controllers: [UserController],
 })
 export class UserModule {}
