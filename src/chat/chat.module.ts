a2b05821716433c0c4100bffcf87b85cabc2efc4
glennprays <glennsagala@gmail.com>
2024-04-03 14:39:44 +0700

    config: socket io

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './core/chat.service';
 import { RoomService } from './features/rooms/room.service';
 import { NotificationService } from './features/notifications/notification.service';
+import { ChatGateway } from './core/chat-gateway';
 
 @Module({
-    providers: [ChatService, RoomService, NotificationService],
+    providers: [ChatService, RoomService, NotificationService, ChatGateway],
 })
 export class ChatModule {}
