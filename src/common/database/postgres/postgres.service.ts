5071ea8da3ef5df09510a73fe2b281d17c063005
glennprays <glennsagala@gmail.com>
2024-04-15 11:12:07 +0700

    update logger

--- src/common/database/postgres/postgres.service.ts
+++ src/common/database/postgres/postgres.service.ts
@@ -1,11 +1,17 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+    INestApplication,
+    Injectable,
+    Logger,
+    OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '../../../../prisma/postgres/generated/client';
 
 @Injectable()
 export class PostgresService extends PrismaClient implements OnModuleInit {
+    private logger = new Logger('PostgresModule');
     async onModuleInit() {
         await this.$connect();
-        console.log('Connection to Postgres...');
+        this.logger.log('Connection to Postgres...');
     }
 
     async enableShutdownHooks(app: INestApplication) {
